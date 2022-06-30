3ccfb5bf53d7f749037df7967219da6851a94e46
MatuuraNobuki <matsuuranobuki@gmail.com>
2022-06-30 16:57:51 +0900

    プレイヤー機能の追加

--- js/app.js
+++ js/app.js
@@ -7,16 +7,24 @@ var app = new Vue({
     isRecording: false,
     recorder: null,
     stream: null,
+    audio: null,
+    duration: '00:00',
+    current: '00:00',
     audioData: [],
     audioUrl: null,
     speech: null,
     interimTranscript: '',
     finalTranscript: '',
-    transcript: '',
     recognition: '',
     ActivateBtn: true,
+    radius: 50,
+    context: null,
+    showplayer: false,
+    dateTime: '',
+    isPlayingAudio: false,
   },
   methods: {
+
     switchRecordingState: function () {
       this.isRecording = !this.isRecording;
       if (this.isRecording) {
@@ -27,24 +35,28 @@ var app = new Vue({
     },
 
     startRecording: async function () {
+      this.finalTranscript = '';
       await this.operateRecordingSystem();
       this.hideBtn = true;
+      this.radius = 25;
       this.transcription();
       setTimeout(() => {
         this.hideBtn = false;
       }, 600);
       this.recognition.start();
-      this.btnMsg = '録音中'
-
+      this.btnMsg = ''
+      this.showplayer = false;
       this.recorder.start();
       this.audioData = [];
+      this.loadProc();
     },
 
     stopRecording: function () {
       this.transcription();
       this.recognition.stop();
-      this.btnMsg = '録音する'
+      this.btnMsg = ''
       this.hideBtn = true;
+      this.radius = 50;
       this.recorder.stop();
       setTimeout(() => {
         this.stream.getTracks()[0].stop();
@@ -72,7 +84,19 @@ var app = new Vue({
       const url = URL.createObjectURL(audioBlob);
       this.audioUrl = url;
     },
+    loadProc: function () {
+      const now = new Date();
+      const dayOfWeekStrJP = ["日", "月", "火", "水", "木", "金", "土"];
+      const year = now.getFullYear();
+      const month = ("0" + (now.getMonth() + 1)).slice(-2);
+      const date = ("0" + now.getDate()).slice(-2);
+      const week = dayOfWeekStrJP[now.getDay()];
+      const hour = ("0" + now.getHours()).slice(-2);
+      const min = ("0" + now.getMinutes()).slice(-2);
+      const sec = ("0" + now.getSeconds()).slice(-2);
 
+      this.dateTime = year + "年" + month + "月" + date + "日" + week + "曜日" + hour + ":" + min + ":" + sec;
+    },
     transcription: function () {
       this.recognition.onresult = (e) => {
         for (let i = e.resultIndex; i < e.results.length; i++) {
@@ -84,9 +108,39 @@ var app = new Vue({
             this.interimTranscript = transcript;
           }
         }
-        this.transcript = this.finalTranscript + this.interimTranscript;
       }
-    }
+    },
+    controllAudio: function () {
+      this.isPlayingAudio = !this.isPlayingAudio;
+      if (this.isPlayingAudio) {
+        this.audio.pause();
+      } else {
+        this.audio.play();
+      }
+    },
+    getDuration: function () {
+      this.audio.currentTime = 0
+      this.audio.removeEventListener('timeupdate', this.getDuration)
+      console.log(this.audio.duration)
+    },
+    playTime: function (t) {
+      let hms = ''
+      const h = t / 3600 | 0
+      const m = t % 3600 / 60 | 0
+      const s = t % 60
+      const z2 = (v) => {
+        const s = '00' + v
+        return s.substr(s.length - 2, 2)
+      }
+      if (h != 0) {
+        hms = h + ':' + z2(m) + ':' + z2(s)
+      } else if (m != 0) {
+        hms = z2(m) + ':' + z2(s)
+      } else {
+        hms = '00:' + z2(s)
+      }
+      return hms
+    },
   },
   mounted() {
     var userAgent = window.navigator.userAgent.toLowerCase();
@@ -99,6 +153,43 @@ var app = new Vue({
     this.recognition.lang = 'ja-JP';
     this.recognition.interimResults = true;
     this.recognition.continuous = true;
-  }
 
+    this.audio = document.getElementById("audioPlayer");
+
+    this.audio.addEventListener('loadedmetadata', async () => {
+      while (this.audio.duration === Infinity) {
+        await new Promise(r => setTimeout(r, 1000));
+        this.audio.currentTime = 1e101;
+      }
+      this.audio.currentTime = 0;
+    })
+
+
+    this.audio.addEventListener("timeupdate", (e) => {
+      const current = Math.floor(this.audio.currentTime);
+      const duration = Math.round(this.audio.duration);
+      if (duration != Infinity && !isNaN(duration)) {
+        if (current == 0) {
+          this.showplayer = true;
+        }
+        this.current = this.playTime(current);
+        this.duration = this.playTime(duration);
+        const percent = Math.round((this.audio.currentTime / this.audio.duration) * 1000) / 10;
+        document.getElementById('seekbar').style.backgroundSize = percent + '%';
+      }
+    })
+    const seekbar = document.getElementById('seekbar');
+    seekbar.addEventListener('click', (e) => {
+      const duration = Math.round(this.audio.duration);
+      if (!isNaN(duration)) {
+        const mouse = e.pageX
+        const element = document.getElementById('seekbar');
+        const rect = element.getBoundingClientRect();
+        const position = rect.left + window.pageXOffset;
+        const offset = mouse - position;
+        const width = rect.right - rect.left;
+        this.audio.currentTime = Math.round(duration * (offset / width));
+      }
+    })
+  }
 })
